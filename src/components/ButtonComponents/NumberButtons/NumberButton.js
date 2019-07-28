bc9736b3993596f4dba309c2ccda27a526fa6b9f
ajohnson1031 <aaron.johnson1031@gmail.com>
2019-07-28 17:11:14 -0400

    MVP done

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = props => {
+  const buttonClass = props.num === "0" ? "double" : null;
   return (
-    <>
+    <button className={buttonClass}>
+      {props.num}
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    </button>
   );
 };
+
+export default NumberButton;
